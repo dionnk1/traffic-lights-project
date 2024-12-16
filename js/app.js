c182fbd663f4b12273a3f92266c615c3b065c8f2
ediz.suleyman@beriflapp.com <ediz.suleyman@beriflapp.com>
2024-12-16 10:44:29 +0100

    feedback

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
+/*
+ * When you have a repetetive operation, try to convert it to a function. This will help you to minimize the necessary code changes on updates.
+ * Imagine a case where you will need to apply a change that will affect all of them, in the case like below, you will need to go over each one by
+ * one to change. But if you convert them to a function, you apply that change in the function and all of them will be using this advantage
+ * to update without needing to make changes on each of them seperately.
+ */
 let redBtn = document.querySelector('.red');
 let orangeBtn = document.querySelector('.orange');
 let greenBtn = document.querySelector('.green');
 let dotButton = document.querySelector('.dot');
-
+/* Be consistent with variable naming. We usually prefer longer naming, like someButton. But for the cases like: text, label, 
+* button it is ok to use them as: txt, lbl, btn. But make sure to use the same format on all of them. For instance:
+* redBtn, orangeBtn, greenBtn, dotBtn
+* OR
+* redButton, orangeButton, greenButton, dotButton
+*/
 let alertInfo = document.querySelector(".alert-info");
 let alertType = document.querySelector('.alert-type');
 let alertDescription = document.querySelector('.alert-description');
@@ -14,7 +25,6 @@ const resetButtonStyles = () => {
     orangeBtn.style.backgroundColor = 'rgb(41, 43, 44)';
     greenBtn.style.backgroundColor = 'rgb(41, 43, 44)';
     dotButton.style.backgroundColor = 'rgb(41, 43, 44)';
-
 }
 
 const setAlert = (bgColor, textColor, alertTypeText, alertDescText) => {
@@ -45,6 +55,10 @@ const handleButtonClick = (btn, color, alertColor, alertTypeText, alertDescText,
     });
 }
 
+/*
+* I debbugged this section below, it triggered 4 clicks on initial run. Not quite sure why this is needed?
+* Make sure to not trigger any unclicked operation forcefully.
+*/
 handleButtonClick(redBtn, '#dc3545', '#dc3545', 'Danger', 'Do NOT pass', '.red');
 handleButtonClick(orangeBtn, '#fd7e14', '#fd7e14', 'Warning!', 'You probably should NOT pass.', '.orange');
 handleButtonClick(greenBtn, '#198754', '#198754', 'Safe.', 'You can pass now.', '.green');
